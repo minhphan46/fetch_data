a5880f7f964e8d6427a41125176c64c3cc748626
minhphan46 <21522345@gm.uit.edu.vn>
2023-01-07 22:57:52 +0700

    update getX

---
+++ lib/controllers/controller.dart
@@ -0,0 +1,17 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../models/user.dart';
+
+class Controller extends GetxController {
+  final String apiUrl = "https://gorest.co.in/public/v1/users";
+
+  Future<List<User>> fetchUsers() async {
+    var response = await http.get(Uri.parse(apiUrl));
+    return (json.decode(response.body)['data'] as List)
+        .map((e) => User.fromJson(e))
+        .toList();
+  }
+}
